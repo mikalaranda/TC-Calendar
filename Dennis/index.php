7709a57f0b0ec87017a546bc201e34c1c3da20f7
Dennis Chen <c.w.dennis@gmail.com>
2014-02-12 12:33:05 -0800

    modal popup when select day

---
+++ Dennis/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <!-- Bootstrap -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->  
+    <!-- end Bootstrap -->
+
+<link href='./assets/css/fullcalendar.css' rel='stylesheet' />
+<link href='./assets/css/fullcalendar.print.css' rel='stylesheet' media='print' />
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://code.jquery.com/jquery.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="./assets/js/bootstrap.min.js"></script>
+<script src='./assets/js/jquery-ui.custom.min.js'></script>
+<script src='./assets/js/fullcalendar.min.js'></script>
+
+<script>
+
+	$(document).ready(function() {
+	
+		var calendar = $('#calendar').fullCalendar({
+		
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end, allDay) {
+				$('#myModal').modal('show');
+				$('.modal-title').html(start)
+			},
+
+			editable: true,
+			
+			events: "dat-events.php",
+			
+			eventDrop: function(event, delta) {
+				alert(event.title + ' was moved ' + delta + ' days\n' +
+					'(should probably update your database)');
+			},
+			
+			loading: function(bool) {
+				if (bool) $('#loading').show();
+				else $('#loading').hide();
+			}
+			
+		});
+		
+	});
+
+</script>
+<style>
+
+	body {
+		margin-top: 40px;
+		text-align: center;
+		font-size: 14px;
+		font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+		}
+		
+	#loading {
+		position: absolute;
+		top: 5px;
+		right: 5px;
+		}
+
+	#calendar {
+		width: 900px;
+		margin: 0 auto;
+		}
+
+</style>
+</head>
+<body>
+<div id='loading' style='display:none'>loading...</div>
+<div id='calendar'></div>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body">
+        Nothing in here yet...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
