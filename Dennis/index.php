67e193192d87ded0920470f7df016fb6176c1ee2
Dennis Chen <c.w.dennis@gmail.com>
2014-02-14 02:39:01 -0800

    Added an ugly progress bar...

--- Dennis/index.php
+++ Dennis/index.php
@@ -62,8 +62,12 @@ $(document).ready(function() {
 		},
 
 		loading: function(bool) {
-			if (bool) $('#loading').show();
-			else $('#loading').hide();
+			if (bool){
+				$('.progress').show();
+			}
+			else{
+				$('.progress').hide();
+			}
 		}
 
 	});
@@ -94,7 +98,9 @@ body {
 </style>
 </head>
 <body>
-	<div id='loading' style='display:none'>loading...</div>
+	<div class='progress progress-striped active' style='display:none'>
+		<div class='progress-bar'  role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'></div>
+	</div>
 	<div id='calendar'></div>
 	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		<div class="modal-dialog">
